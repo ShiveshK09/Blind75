5eece845f547ff4521b73fca270335547a2aa75a
Shivesh <122783487+ShiveshK09@users.noreply.github.com>
2026-08-16 02:40:54 +0530

    Add solution for longest consecutive sequence problem
    Implemented a solution to find the longest consecutive sequence in an array of integers using a HashSet.

---
+++ Arrays & Hashing/9. Longest Consecutive Sequence.java
@@ -0,0 +1,29 @@
+//https://leetcode.com/problems/longest-consecutive-sequence/
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        Set<Integer> set=new HashSet<>();
+        for(int n:nums)set.add(n);
+        int ans=0;
+        for(int n:set){
+            if(!set.contains(n-1)){
+            int len=1;
+            while(set.contains(n+len)) len++;
+            ans=Math.max(ans,len);
+            }
+        }
+        return ans;
+    }
+}
+/*
+HashSet
+   ↓
+Is n-1 missing?
+   ↓ YES
+START
+   ↓
+Check n+1, n+2, n+3...
+   ↓
+Length
+   ↓
+MAX
+*/
